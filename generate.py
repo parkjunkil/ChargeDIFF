940059f726f5d234561665198418037001fcdc45
parkjunkil <88761984+parkjunkil@users.noreply.github.com>
2025-11-27 10:16:49 +0900

    Update generate.py

--- generate.py
+++ generate.py
@@ -25,8 +25,8 @@ def generate_structures(model, batch_size, num_batch, target, save_dir):
     opt.vq_cfg="./configs/vqvae.yaml"
     opt.vq_ckpt="./saved_ckpt/vqvae.pth"
         
-    assert model in [None, 'uncond', 'magnetic_density', 'bandgap'], \
-    f"Unexpected model value '{model}' provided. Please choose from 'uncond', 'magnetic_density' or 'bandgap'"
+    assert model in [None, 'uncond', 'magnetic_density', 'bandgap', 'crystal_density], \
+    f"Unexpected model value '{model}' provided. Please choose from 'uncond', 'magnetic_density', 'bandgap', or 'crystal density'"
 
         
     if model == None or model =='uncond':
@@ -71,6 +71,10 @@ def generate_structures(model, batch_size, num_batch, target, save_dir):
             opt.ckpt = './saved_ckpt/bandgap.pth'
             scaler = torch.load('./saved_ckpt/scaler_bandgap.pth')            
 
+        elif model == 'density':
+            opt.model='chargediff_density'
+            opt.ckpt = './saved_ckpt/density.pth'
+            scaler = torch.load('./saved_ckpt/scaler_density.pth')      
                 
         ChargeDIFF = create_model(opt)
         cprint(f'[*] "{ChargeDIFF.name()}" loaded.', 'cyan')
